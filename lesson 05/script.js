25eea2442bf9adcd66e2f14995b9f9bed5e8a1d8
Victoria <victoria.ejvk@gmail.com>
2021-03-03 14:39:48 +0300

    Это первый комит

---
+++ lesson 05/script.js
@@ -0,0 +1,13 @@
+'use strict'
+var one = 78;
+
+console.log(one);
+
+//alert('Hello World!');
+
+/* let answer = confirm("Уляля");
+console.log(answer); */
+
+let answer = prompt("Уляля");
+console.log(answer);
+console.log(typeof(null));
